2533cb1360167ce92f6ced34f5acaca6eeda9a92
aston4ever <aston4ever@gmail.com>
2024-01-31 14:35:20 +0300

    feat: add local score reset

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 'use strict';
 
-let score0El = document.querySelector("#score--0");
-let score1El = document.querySelector("#score--1");
-let diceEl = document.querySelector(".dice");
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById("score--1");
+const diceEl = document.querySelector(".dice");
 const btnRoll = document.querySelector(".btn--roll");
 const btnNew  = document.querySelector(".btn--new");
 const btnHold = document.querySelector(".btn--hold");
-const current0El = document.querySelector("#current--0");
-const current1El = document.querySelector("#current--1");
-
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
+
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener("click", ()=> {
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -23,9 +27,12 @@ btnRoll.addEventListener("click", ()=> {
 
     if (dice !== 1) {
       currentScore += dice;
-      currentScore += dice;
-      current0El.textContent = currentScore;
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
-
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+      currentScore = 0;
+      activePlayer = activePlayer === 0 ? 1 : 0;
+      player0El.classList.toggle('player--active');
+      player1El.classList.toggle('player--active');
     }
 })
