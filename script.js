b6c0e0bc46201b968c86b27e638fef3d3f1f3d59
aston4ever <aston4ever@gmail.com>
2024-02-01 12:01:37 +0300

    fix & refactor

--- script.js
+++ script.js
@@ -10,15 +10,26 @@ const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+let scores, currentScore, activePlayer, playing;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
 
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
 
 function switchPlayer() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -47,7 +58,7 @@ btnRoll.addEventListener('click', () => {
 btnHold.addEventListener('click', () => {
   if (playing) {
     scores[activePlayer] += currentScore;
-    document.querySelector(`#score${activePlayer}`).textContent = score[activePlayer];
+    document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
 
     if (scores[activePlayer] >= 100) {
       playing = false;
@@ -58,5 +69,19 @@ btnHold.addEventListener('click', () => {
       switchPlayer();
     }
   }
-
 });
+
+btnNew.addEventListener("click", ()=> {
+  activePlayer = 0;
+  player0El.classList.toggle('player--active');
+  scores[0] = 0;
+  scores[1] = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+})
